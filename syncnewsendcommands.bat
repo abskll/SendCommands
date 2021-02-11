a16c787ee3c6f8754f7ba7248d847ba31f7ee810
Anthony Bulvan <abulva2@gmail.com>
2021-02-11 14:33:59 -0600

    Add files via upload
    final?

--- syncnewsendcommands.bat
+++ syncnewsendcommands.bat
@@ -2,6 +2,6 @@ cd C:\Users\abulv\OneDrive\Documents\GItHub\SendCommands
 xcopy C:\Users\abulv\source\repos\SendCommands\SendCommandsSetup\Debug /e
 cd C:\Users\abulv\OneDrive\Documents\GItHub\SendCommands
 git add -A
-git commit -m "added personal information"
+git commit -m "fixed url problem"
 git remote add origin https://github.com/abskll/SendCommands.git
 git push origin main
