c48dd8fe2cb38764b78369c74c17964aa03addb4
Anthony Bulvan <abulva2@gmail.com>
2021-02-10 19:48:39 -0600

    added personal information

---
+++ syncnewsendcommands.bat
@@ -0,0 +1,7 @@
+cd C:\Users\abulv\source\repos\SendCommands\SendCommandsSetup\Debug
+xcopy C:\Users\abulv\OneDrive\Documents\GItHub\SendCommands /e
+cd C:\Users\abulv\OneDrive\Documents\GItHub\SendCommands
+git add -A
+git commit -m "added personal information"
+git remote add origin https://github.com/abskll/SendCommands.git
+git push origin main
