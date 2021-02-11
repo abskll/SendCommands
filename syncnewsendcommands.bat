9394b57ba52a1036e2d4301e955408f55a7c62b1
Anthony Bulvan <abulva2@gmail.com>
2021-02-10 19:54:35 -0600

    added personal information

--- syncnewsendcommands.bat
+++ syncnewsendcommands.bat
@@ -1,5 +1,5 @@
-cd C:\Users\abulv\source\repos\SendCommands\SendCommandsSetup\Debug
-xcopy C:\Users\abulv\OneDrive\Documents\GItHub\SendCommands /e
+cd C:\Users\abulv\OneDrive\Documents\GItHub\SendCommands
+xcopy C:\Users\abulv\source\repos\SendCommands\SendCommandsSetup\Debug /e
 cd C:\Users\abulv\OneDrive\Documents\GItHub\SendCommands
 git add -A
 git commit -m "added personal information"
